f5d7e0bdcb377f686426297caf0d1d9851ef1014
algokun <themightyking117@gmail.com>
2021-02-15 17:21:20 +0530

    updated twitter card

--- src/modules/head.cpp
+++ src/modules/head.cpp
@@ -15,7 +15,7 @@ string html_head(Config* conf, string text) {
          "'><meta name='viewport' content='width=device-width, initial-scale=1.0'>"
          "<meta name='robots' content='index,follow' />"
          "<meta name='twitter:card' content='summary_large_image' />"
-         "<meta name='googlebot' content='index,follow' /><meta name='twitter:card' content='summary'><meta "
+         "<meta name='googlebot' content='index,follow' /><meta "
          "name='twitter:site' content='" +
          conf->name + "'><meta name='twitter:title' content='" + conf->name +
          "'><meta name='twitter:description' content='" + conf->description +
